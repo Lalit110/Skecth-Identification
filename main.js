5b6ed138412cd92c12640ce93bc861bc6029bfae
Lalit110 <83270963+Lalit110@users.noreply.github.com>
2022-05-06 16:49:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function setup(){
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classify_canvas);
+    synth = window.speechSynthesis;
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classify_canvas(){
+    classifier.classify(canvas, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("label").innerHTML = "Label :" + results[0].label;
+        document.getElementById("confidence").innerHTML = "Confidence :" + Math.round(results[0].confidence * 100) + "%";
+        utterThis = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+    }
+}
